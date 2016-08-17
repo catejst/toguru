7a63604ace5272b23a2037ddb93b0e3ff783935d
Andreas Schroeder <aschroeder@autoscout24.com>
2016-08-17 18:51:35 +0200

    Adding event publishing facility
    - Introducing Event trait and EventPublishing convenience trait.
    - Whitespace change in logger config
    - Event publishing supports only primitive fields. Serialization of complex types
      to json is not supported.

---
+++ app/dimmer/logging/EventPublisher.scala
@@ -0,0 +1,38 @@
+package dimmer.logging
+
+import net.logstash.logback.marker.LogstashMarker
+import net.logstash.logback.marker.Markers.appendEntries
+import org.slf4j.{Logger, LoggerFactory}
+
+trait Event {
+
+  def eventName: String
+
+  def eventFields: Seq[(String, Any)]
+}
+
+trait EventPublishing {
+  val publisher = new EventPublisher
+}
+
+object EventPublisher {
+  val eventLogger: Logger = LoggerFactory.getLogger("event-logger")
+}
+
+class EventPublisher {
+  import EventPublisher._
+  import collection.JavaConverters._
+
+  def event(name: String, fields: (String, Any)*): Unit = eventLogger.info(markers(name, fields), "")
+
+  def event(name: String, exception: Throwable, fields: (String, Any)*): Unit = {
+    val eventMarkers = markers(name, fields :+ ("exception_type" -> exception.getClass.getName))
+
+    eventLogger.info(eventMarkers, exception.getMessage, exception)
+  }
+
+  def event(e: Event): Unit = event(e.eventName, e.eventFields: _*)
+
+  private def markers(name: String, fields: Seq[(String, Any)]): LogstashMarker =
+    appendEntries(Map(fields: _*).updated("@name", name).asJava)
+}
