d385cf1db8d9e91145c26f544ac2ecda1decb123
Andreas Schroeder <andreas-schroeder@users.noreply.github.com>
2017-03-16 10:28:33 +0100

    Adding CORS filter (#54)

--- app/toguru/filters/Filters.scala
+++ app/toguru/filters/Filters.scala
@@ -4,15 +4,16 @@ import javax.inject.Inject
 
 import akka.stream.Materializer
 import com.kenshoo.play.metrics.Metrics
-import play.api.http.HttpFilters
+import play.api.http.{DefaultHttpFilters, HttpFilters}
 import play.api.mvc._
+import play.filters.cors.CORSFilter
 import play.filters.gzip.GzipFilter
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
-class Filters @Inject()(gzip: GzipFilter, counter: ErrorCounterFilter) extends HttpFilters {
-  val filters = Seq(gzip, counter)
+class Filters @Inject()(gzip: GzipFilter, counter: ErrorCounterFilter, corsFilter: CORSFilter) extends HttpFilters {
+  val filters = Seq(gzip, counter, corsFilter)
 }
 
 class ErrorCounterFilter @Inject() (implicit val mat: Materializer, ec: ExecutionContext, metrics: Metrics) extends Filter {
